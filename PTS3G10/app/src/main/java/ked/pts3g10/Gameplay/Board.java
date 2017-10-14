0b1fa1778efca87215079250815d05aa39d2b97b
ked <kedthunder@gmail.com>
2017-10-14 20:15:26 +0200

    Génération du board maintenant procédurale, meilleure maitrise de celui ci

--- PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/Board.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/Board.java
@@ -1,9 +1,14 @@
 package ked.pts3g10.Gameplay;
 
 
+import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import ked.pts3g10.GameActivity;
+import ked.pts3g10.Interface.Case;
 import ked.pts3g10.R;
 
 public class Board {
@@ -12,10 +17,29 @@ public class Board {
     private Player player;
     private Player adversary;
 
+    private LinearLayout V1,V2,V3,V4,V5;//V pour verical, le nombre pour le numéro de colonne
+    private ArrayList<Case> cases;
+
     public Board(GameActivity context, Player player, Player adversary){
         this.context = context;
         this.player = player;
         this.adversary = adversary;
+        this.cases = new ArrayList<Case>();
+
+        V1 = (LinearLayout) context.findViewById(R.id.V1);
+        V2 = (LinearLayout) context.findViewById(R.id.V2);
+        V3 = (LinearLayout) context.findViewById(R.id.V3);
+        V4 = (LinearLayout) context.findViewById(R.id.V4);
+        V5 = (LinearLayout) context.findViewById(R.id.V5);
+        initLayout(V1);
+        initLayout(V2);
+        initLayout(V3);
+        initLayout(V4);
+        initLayout(V5);
+
+        //On met les chateaux
+        getCaseWithLinearLayoutNumber(2,0).setCardThumbnail(R.drawable.castle);
+        getCaseWithLinearLayoutNumber(2,4).setCardThumbnail(R.drawable.castle);
 
         /*Init des text view */
         context.updateText(R.id.ADVCardsLeftText,adversary.getDeck().getCardList().size()); // Par défaut pour le moment
@@ -25,5 +49,17 @@ public class Board {
         context.updateText(R.id.PlayerCardsLeftText,player.getDeck().getCardList().size());
         context.updateText(R.id.PlayerCrystalsText,player.getCrystals());
         context.updateText(R.id.PlayerNick,player.getNickName());
+    }
+
+    public void initLayout(LinearLayout V){
+        for(int i = 0; i < 5; ++i){
+            Case c = new Case(context);
+            V.addView(c);
+            cases.add(c);
+        }
+    }
+
+    public Case getCaseWithLinearLayoutNumber(int linearLayoutNumber, int caseNumber){
+        return cases.get(linearLayoutNumber*5+caseNumber);
     }
 }
--- PTS3G10/app/src/main/java/ked/pts3g10/Interface/Case.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/Interface/Case.java
@@ -6,13 +6,54 @@ L'intérêt ici est d'extend FrameLayout et de construire notre interface procé
 
  */
 
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import ked.pts3g10.GameActivity;
+import ked.pts3g10.R;
 
 public class Case extends FrameLayout{
 
+    private ImageView grass;
+    private ImageView cardThumbnail; //Pas inspiré pour ce nom, représente l'image de la carte sur la case
+    private GameActivity context;
+    private boolean isOccupied;
+
     public Case(GameActivity context){
         super(context);
+        this.context = context;
+        isOccupied=false;
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT,1);
+        this.setLayoutParams(lp);
+
+
+        /* setPadding n'utilise que les pixels
+            on va donc faire une conversion grâce à la densité de l'écran
+            pour avoir un équivalent dp mais en pixel
+         */
+        int paddingPixel = 2;
+        float density = context.getResources().getDisplayMetrics().density;
+        int paddingDp = (int)(paddingPixel * density);
+        this.setPadding(paddingDp,paddingDp,paddingDp,paddingDp);
+
+        grass = new ImageView(context);
+        grass.setBackgroundResource(R.drawable.grass);
+        this.addView(grass);
+
+        cardThumbnail = new ImageView(context);
+        this.addView(cardThumbnail);
+    }
+
+    public boolean setCardThumbnail(int id){
+        if(!isOccupied){ // On ne veut pas supperposer les cartes !
+            cardThumbnail.setBackgroundResource(id);
+            cardThumbnail.bringToFront();
+            isOccupied = true;
+            return true;
+        }else return false;
     }
 }
