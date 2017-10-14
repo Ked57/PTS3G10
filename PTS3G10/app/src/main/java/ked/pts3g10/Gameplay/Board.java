84e359fd4430daf792c6159689044e7c1552ddf5
ked <kedthunder@gmail.com>
2017-10-14 20:50:47 +0200

    Implémentation d'un système rudimentaire de gestion des rounds

--- PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/Board.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/Board.java
@@ -1,11 +1,16 @@
 package ked.pts3g10.Gameplay;
 
 
+import android.graphics.Color;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ked.pts3g10.GameActivity;
 import ked.pts3g10.Interface.Case;
@@ -17,8 +22,17 @@ public class Board {
     private Player player;
     private Player adversary;
 
+    private final int DEFAULT_SECONDS = 60;//Temps au tour par défaut
+
+    private int seconds; //Temps du tour
+    private boolean endRound;
+    private boolean playersTurn; // true -> le joueur, false -> l'adversaire
+
+
     private LinearLayout V1,V2,V3,V4,V5;//V pour verical, le nombre pour le numéro de colonne
     private ArrayList<Case> cases;
+
+    private Button endRoundButton;
 
     public Board(GameActivity context, Player player, Player adversary){
         this.context = context;
@@ -49,6 +63,19 @@ public class Board {
         context.updateText(R.id.PlayerCardsLeftText,player.getDeck().getCardList().size());
         context.updateText(R.id.PlayerCrystalsText,player.getCrystals());
         context.updateText(R.id.PlayerNick,player.getNickName());
+
+        endRoundButton = (Button) context.findViewById(R.id.EndButton);
+        endRoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endRound = true;
+            }
+        });
+        seconds = DEFAULT_SECONDS;
+        endRound = false;
+        playersTurn = false; // Valeur acheminée du serveur, implémenté plus tard
+
+        newRound();
     }
 
     public void initLayout(LinearLayout V){
@@ -61,5 +88,50 @@ public class Board {
 
     public Case getCaseWithLinearLayoutNumber(int linearLayoutNumber, int caseNumber){
         return cases.get(linearLayoutNumber*5+caseNumber);
+    }
+
+    public void newRound(){
+        seconds = DEFAULT_SECONDS;
+        endRound = false;
+        playersTurn = !playersTurn;
+
+        if(playersTurn){
+            endRoundButton.setBackgroundColor(Color.rgb(76,49,35));
+            endRoundButton.setClickable(true);
+        }else {
+            endRoundButton.setBackgroundColor(Color.GRAY);
+            endRoundButton.setClickable(false);
+        }
+
+        //Declare the timer
+        Timer t = new Timer();
+        //Set the schedule function and rate
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                context.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        TextView tv = (TextView) context.findViewById(R.id.Timer);
+                        tv.setText(String.valueOf(seconds));
+                        seconds -= 1;
+
+                        if(seconds == 0 || endRound)
+                        {
+                            newRound();
+                            cancel();
+                        }
+
+
+
+                    }
+
+                });
+            }
+
+        }, 0, 1000);
+
     }
 }
