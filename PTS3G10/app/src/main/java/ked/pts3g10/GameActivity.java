6a6e2e9cf584b68d31e8bd9a158e331185e7192a
ked <kedthunder@gmail.com>
2017-10-18 10:10:34 +0200

    Ajout d'un menu permettant de passer au tour suivant (pratique pour les tests)

--- PTS3G10/app/src/main/java/ked/pts3g10/GameActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/GameActivity.java
@@ -2,6 +2,9 @@ package ked.pts3g10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import ked.pts3g10.DB.CardDB;
@@ -25,6 +28,24 @@ public class GameActivity extends AppCompatActivity {
         temgr = new GameTouchEventMgr(this);
 
         ActivityMgr.gameActivity = this;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.newRound:
+                board.newRound();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void updateText(int id,String str){
