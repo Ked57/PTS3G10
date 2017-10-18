93a6618b63d11e4e1c08c7e5b963a368f420c7b8
ked <kedthunder@gmail.com>
2017-10-18 09:49:02 +0200

    Les cartes jouées sont maintenant déduites du deck

--- PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/PlayerAction.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/Gameplay/PlayerAction.java
@@ -24,6 +24,7 @@ public class PlayerAction {
     public void placeBoardCard(Card card, Case new_case) {
         new_case.setCard(card);
         player.setCrystals(player.getCrystals()-card.getCrystalCost());
+        player.getDeck().getCardList().remove(card);
         ActivityMgr.gameActivity.getBoard().updateTexts();
         resetActionState();
     }
