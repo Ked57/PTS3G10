43c8061ab42b263a3190c91ed5b3ddbd750fff99
Warlocklogan <logan.gonet@hotmail.fr>
2017-11-23 17:20:07 +0100

    Passage Fenetre a une autre + Quelque verif sur les champs

--- PTS3G10/app/src/main/java/ked/pts3g10/ConnectionActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/ConnectionActivity.java
@@ -1,19 +1,74 @@
 package ked.pts3g10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ConnectionActivity extends AppCompatActivity {
 
-    private EditText pseudo;
-    private EditText password;
+    private EditText pseudo,password;
+    private Button connection, inscription;
+    private String stringPseudo,stringPassword;
+    private Boolean connecter=true;//<<<<<<<<<<<<<<<-----------------------Variable pour la connection sur vrais pour le test
+    private ConnectionActivity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        //On lie les élèments de l'interface a nos objets
         pseudo = (EditText)findViewById(R.id.pseudo);
         password =(EditText)findViewById(R.id.password);
+        connection = (Button) findViewById(R.id.buttonconnection);
+        inscription = (Button) findViewById(R.id.buttoninscription);
+        context=this;
+
+        //Ouvre l'activite du jeux et envoie les informations de connection a la bdd
+        connection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringPseudo=pseudo.getText().toString();
+                stringPassword=password.getText().toString();
+                //Verifie que les champs ne soit pas vide
+                if(stringPassword.isEmpty()|stringPseudo.isEmpty()){
+                    Toast toast =Toast.makeText(context,R.string.toastChampVide,Toast.LENGTH_LONG);
+                    toast.show();
+                }else{
+                    // envoyer les info vers la bdd
+                    //
+                    //
+                    //Ajout de la condition que doit être identifier
+                    if(connecter){
+                        Intent launch = new Intent(context,LaunchActivity.class);
+                        //Envoie de variable a l'autre fenetre ?
+                        //
+                        //
+                        startActivity(launch);
+                        //Empeche le retour en arrière car on ferme l'activité après connection
+                        context.finish();
+                    }
+                }
+            }
+        });
+
+        //Button inscription ouvre l'activitie inscription
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ouvre la fenetre d'inscription
+                Intent inscription = new Intent(context,InscriptionActivity.class);
+                startActivity(inscription);
+                //On ne ferme pas cet activity pour eviter de la rapeller après l'inscription
+            }
+        });
+
     }
+
+
+
 }
--- PTS3G10/app/src/main/java/ked/pts3g10/InscriptionActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/InscriptionActivity.java
@@ -2,22 +2,75 @@ package ked.pts3g10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InscriptionActivity extends AppCompatActivity {
 
-    private EditText pseudoIns;
-    private EditText passwordIns;
-    private EditText email;
-    private EditText passwordConf;
+    private EditText pseudoIns,passwordIns,email,passwordConf;
+    private Button creeCompte,annuler;
+    private InscriptionActivity context;
+    private String stringPseudo,stringPassword,stringEmail,stringPasswordconf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        //Lie les elements de l'activity inscription au objet java
         pseudoIns = (EditText)findViewById(R.id.pseudoIns);
         passwordIns = (EditText)findViewById(R.id.passwordIns);
         email = (EditText)findViewById(R.id.email);
         passwordConf = (EditText)findViewById(R.id.mdpconf);
+        creeCompte = (Button) findViewById(R.id.buttoncreationcompte);
+        annuler = (Button) findViewById(R.id.buttonannuler);
+        context=this;
+
+        //affiche la fenetre d'inscription et envoie les donnée d'inscription a la bdd
+        creeCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringPseudo=pseudoIns.getText().toString();
+                stringPassword=passwordIns.getText().toString();
+                stringPasswordconf=passwordConf.getText().toString();
+                stringEmail=email.getText().toString();
+                //Verifie que les champs ne soit pas vide
+                if(stringPseudo.isEmpty()|stringPassword.isEmpty()|stringPasswordconf.isEmpty()|stringEmail.isEmpty()){
+                    Toast toast = Toast.makeText(context,R.string.toastChampVide,Toast.LENGTH_LONG);
+                    toast.show();
+                }else{
+                    //verifie si le "password" correspond bien a "confirmed password"
+                    if(stringPassword.equals(stringPasswordconf)){
+                        //Envoie des informations a la base de donnée et verifie si elles existes
+                        //
+                        //
+                        //
+                        //Affiche un toast pour dire création du compte réussie
+                        Toast toast = Toast.makeText(context,R.string.toastCreationDuCompte,Toast.LENGTH_LONG);
+                        toast.show();
+                        //Ferme la fenetre pour retourner sur l'ecran de connection
+                        context.finish();
+
+                    }else{
+                        Toast toast = Toast.makeText(context,R.string.toastConfirmationPassword,Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+
+
+            }
+        });
+
+        //affiche la fenetre d'inscription
+        annuler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ferme la fenetre pour retourner sur l'ecran de connection
+                context.finish();
+            }
+        });
+
     }
 }
