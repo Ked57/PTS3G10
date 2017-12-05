984bf8a8c6ce6c880f3df4807b1704ae7abdc80d
i160417 <logan.gonet@hotmail.fr>
2017-12-05 15:57:16 +0100

    Petit Test

--- PTS3G10/app/src/main/java/ked/pts3g10/CreditActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/CreditActivity.java
@@ -3,6 +3,7 @@ package ked.pts3g10;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import ked.pts3g10.R;
 
@@ -12,9 +13,14 @@ import ked.pts3g10.R;
  */
 
 public class CreditActivity extends AppCompatActivity {
+
+    private ImageView logoIut;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        logoIut = (ImageView)findViewById(R.id.CreditIut);
+        logoIut.setColorFilter(R.color.colorBlue);
         setContentView(R.layout.activity_credits);
 
     }
