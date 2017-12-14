39e749dd1ea11bae5e07d5828b9d0b20d3368962
Xanderux <kmignot@hotmail.fr>
2017-12-14 17:46:03 +0100

    XML création deck
    Ajout interface création d'un deck

--- PTS3G10/app/src/main/java/ked/pts3g10/ConstructionDeckActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/ConstructionDeckActivity.java
@@ -1,6 +1,7 @@
 package ked.pts3g10;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,24 +11,42 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import ked.pts3g10.Gameplay.CardPackage.Card;
+import ked.pts3g10.Gameplay.Player;
+
 public class ConstructionDeckActivity extends AppCompatActivity {
 
     private RelativeLayout espacCarte;
-    private ImageButton deck;
+    private Button selectionner;
+    private ImageButton left_arrow, right_arrow;
+    private ArrayList<Card> listCardPlayer;
 
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_construction_deck);
-
-      ImageButton deck = findViewById(R.id.deck);
-
-
-
+        selectionner = (Button) findViewById(R.id.selectionner);
+        left_arrow = (ImageButton) findViewById(R.id.left_arrow);
+        right_arrow = (ImageButton) findViewById(R.id.right_arrow);
 
 
+
+
+
+
+
+
+    }
+
+
+    /***
+     *
+     * @param player
+     */
+    public void afficher( Player player){
 
 
 
