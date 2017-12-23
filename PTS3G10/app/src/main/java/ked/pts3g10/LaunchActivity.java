1706bad4a12ec73891f86b4385244e4fd404f7eb
Ked <Kedthunder@gmail.com>
2017-12-23 18:18:35 +0100

    L'application attend maintenant le chargement des cartes avant de lancer une partie

--- PTS3G10/app/src/main/java/ked/pts3g10/LaunchActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/LaunchActivity.java
@@ -45,43 +45,6 @@ public class LaunchActivity extends AppCompatActivity {
 
         getDistantCards();
 
-            /* TODO: récupérer les cartes via le serveur (Json) */
-
-                /*ImageView bg = new ImageView(this);
-                bg.setBackgroundResource(R.drawable.castle);
-                bg.setTag(R.drawable.castle);//A ne pas oublier
-                ImageView thmbn = new ImageView(this);
-                thmbn.setBackgroundResource(R.drawable.castle);
-                thmbn.setTag(R.drawable.castle);
-                cards.add(new Army("Chateau", "Votre base principale",0,0,0,30,0, bg, thmbn,false));
-
-                ImageView bg4 = new ImageView(this);
-                bg4.setBackgroundResource(R.drawable.castle);
-                bg4.setTag(R.drawable.castle);//A ne pas oublier
-                ImageView thmbn4 = new ImageView(this);
-                thmbn4.setBackgroundResource(R.drawable.castle);
-                thmbn4.setTag(R.drawable.castle);
-                cards.add(new Army("Chateau", "Votre base principale",0,0,0,30,0, bg4, thmbn4,true));
-
-                ImageView bg3 = new ImageView(this);
-                bg3.setBackgroundResource(R.drawable.bow);
-                bg3.setTag(R.drawable.bow);//A ne pas oublier
-                ImageView thmbn3 = new ImageView(this);
-                thmbn3.setBackgroundResource(R.drawable.bow);
-                thmbn3.setTag(R.drawable.bow);
-                cards.add(new Army("Archers", "Une armée d'archers",2,2,2,2,1, bg3, thmbn3,false));
-
-                ImageView bg2 = new ImageView(this);
-                bg2.setBackgroundResource(R.drawable.sword);
-                bg2.setTag(R.drawable.sword);//A ne pas oublier
-                ImageView thmbn2 = new ImageView(this);
-                thmbn2.setBackgroundResource(R.drawable.sword);
-                thmbn2.setTag(R.drawable.sword);
-                cards.add(new Army("Légion","Une armée de légionnaires",1,2,1,2,1,bg2,thmbn2,false));*/
-
-
-
-
 
         findViewById(R.id.playButton).setOnClickListener(new View.OnClickListener() {
              @Override
@@ -94,10 +57,12 @@ public class LaunchActivity extends AppCompatActivity {
         findViewById(R.id.devButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LaunchActivity.this, GameActivity.class);
-                intent.putExtra("adversaryName","Dev");
-                intent.putExtra("starting","true");
-                startActivity(intent);
+                if(cards.size() > 0) {
+                    Intent intent = new Intent(LaunchActivity.this, GameActivity.class);
+                    intent.putExtra("adversaryName", "Dev");
+                    intent.putExtra("starting", "true");
+                    startActivity(intent);
+                }
             }
         });
 
@@ -129,7 +94,7 @@ public class LaunchActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(!adversaryName.equals("")) {
+                        if(!adversaryName.equals("") && cards.size() > 0) {
                             startGame();
                         }
                     }
