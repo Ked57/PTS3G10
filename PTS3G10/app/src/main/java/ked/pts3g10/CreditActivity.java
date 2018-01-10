cfcbd4cf81cc4bd7a05247485f8cca7e620324eb
logan <logan.gonet@hotmail.fr>
2018-01-10 12:10:16 +0100

    Crédit Refonte + Suppression de warning

--- PTS3G10/app/src/main/java/ked/pts3g10/CreditActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/CreditActivity.java
@@ -1,25 +1,48 @@
 package ked.pts3g10;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import ked.pts3g10.R;
 
-
-/**
- * Created by Kévin on 22/10/2017.
- */
-
 public class CreditActivity extends AppCompatActivity {
 
-    private ImageView logoIut;
+    private Button button;
+    private CreditActivity context;
+    private ImageView logo;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        button = findViewById(R.id.buttonCreditToMenu);
+        logo = findViewById(R.id.logo);
+        context=this;
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.finish();
+            }
+        });
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url ="http://www.iut-laval.univ-lemans.fr/fr/index.html";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
 
 
     }
