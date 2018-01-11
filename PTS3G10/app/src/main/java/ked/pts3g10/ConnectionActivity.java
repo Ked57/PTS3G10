573c321c34938f617cf44c1e4f1d44396b65575e
shyndard <GithubMael17>
2018-01-11 17:08:38 +0100

    Maj messages de connexion corrigé

--- PTS3G10/app/src/main/java/ked/pts3g10/ConnectionActivity.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/ConnectionActivity.java
@@ -188,7 +188,7 @@ public class ConnectionActivity extends AppCompatActivity {
             toast.show();
             emptyError = false;
         }else if(unvalidCharError){
-            Toast t = Toast.makeText(context, errorMessage, Toast.LENGTH_LONG);
+            Toast t = Toast.makeText(context,R.string.toastCaractereInvalide,Toast.LENGTH_LONG);
             t.show();
             unvalidCharError = false;
         }
@@ -238,7 +238,7 @@ public class ConnectionActivity extends AppCompatActivity {
         t.cancel();
         t = null;
 
-        Toast t = Toast.makeText(context,R.string.toastConnectionFailed ,Toast.LENGTH_LONG);
+        Toast t = Toast.makeText(context, errorMessage, Toast.LENGTH_LONG);
         t.show();
         token = 0;
         editTextError(pseudo);
