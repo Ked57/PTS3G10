64059a8fc04dd2bb977f586cf34d5e6b9f9244df
Ked <Kedthunder@gmail.com>
2018-01-14 13:28:00 +0100

    Plein de nouvelles cartes

--- PTS3G10/app/src/main/java/ked/pts3g10/Util/ImagesEnum.java
+++ PTS3G10/app/src/main/java/ked/pts3g10/Util/ImagesEnum.java
@@ -16,12 +16,20 @@ public enum ImagesEnum {
     BOW(2, R.drawable.bow),
     STAFF(3, R.drawable.staff),
     SHYNDARD(4, R.drawable.shyndard),
+    KEVIN(5, R.drawable.kevin),
+    KED(6, R.drawable.ked),
+    LOGAN(7, R.drawable.warlock_logan),
+    KNIGHT(8, R.drawable.knight),
     //RED
     CASTLERED(1000, R.drawable.castlered),
     SWORDRED(1001, R.drawable.swordred),
     BOWRED(1002, R.drawable.bowred),
     STAFFRED(1003, R.drawable.staffred),
-    SHYNDARDRED(1004, R.drawable.shyndard);
+    SHYNDARDRED(1004, R.drawable.shyndardred),
+    KEVINRED(1005, R.drawable.kevinred),
+    KEDRED(1006, R.drawable.kedred),
+    LOGANRED(1007, R.drawable.warlock_loganred),
+    KNIGHTRED(1008, R.drawable.knightred);
 
     private int imageId;
     private int drawableId;
